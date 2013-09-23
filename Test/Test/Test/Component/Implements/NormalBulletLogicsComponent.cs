f6be42b81939d40a991b27e0d635322ed8e392d0
giangchau92 <giangchau92@370118cd-89e9-8490-785a-544dbd15d94d>
2013-09-23 17:52:51 +0000

    Xong demo plant and zombie

---
+++ Test/Test/Test/Component/Implements/NormalBulletLogicsComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Test.Component;
+using Test.Objects;
+
+namespace Test.Component.Implements
+{
+    public class NormalBulletLogicsComponent : ILogicsComponent
+    {
+        public void Update(Objects.GameObject owner, GameTime gameTime)
+        {
+            if (owner as NormalBullet == null)
+                throw new Exception("owner is not Bullet");
+
+            NormalBullet bullet = owner as NormalBullet;
+            owner.Position += new Vector2((float)(owner.Velocity.X * gameTime.ElapsedGameTime.TotalSeconds),
+                (float)(owner.Velocity.Y * gameTime.ElapsedGameTime.TotalSeconds));
+
+            // Check with zombie
+            List<GameObject> gameObjects = SCSServices.GetInstance().Game.GameWorld;
+            foreach (var item in gameObjects)
+            {
+                if (item == bullet)
+                    continue;
+
+                if (item.Bound.Intersects(bullet.Bound) && (item as Zombie) != null)
+                {
+                    (item as Zombie).Heath -= 10;
+                    bullet.Alive = false;
+                }
+            }
+        }
+    }
+}
---
+++ Test/Test/Test/Objects/Plant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Test.Objects;
+using Test.Component.Implements;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Test.Objects
+{
+    public enum PlantState
+    {
+        STAND, SHOOT
+    }
+    public class Plant : GameObject
+    {
+        public PlantState State { get; set; }
+
+        public int Heath { get; set; }
+
+        public Plant()
+            :base(new PlantLogicsComponent(), new PlantDrawComponent())
+        {
+            State = PlantState.STAND;
+            Frame = new Rectangle(0, 0, 50, 100);
+            Heath = 100;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Heath <= 0)
+                Alive = false;
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            spriteBatch.DrawString(SCSServices.GetInstance().DebugFont, Heath.ToString(), new Vector2(Position.X, Position.Y - 30), Color.White);
+        }
+    }
+}
