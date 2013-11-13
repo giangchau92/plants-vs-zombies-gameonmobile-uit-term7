515c67809afa0c8f688d78dfbd615115297c26f2
giangchau92 <giangchau92@370118cd-89e9-8490-785a-544dbd15d94d>
2013-11-13 18:45:22 +0000

    Fix physic - logic Zombie trường hợp nhiều con ăn plant 1 lúc

--- trunk/SSCEngine/PlantVsZombie/PlantVsZombie/GameComponents/Behaviors/Zombie/Z_NormalLogicBehavior.cs
+++ trunk/SSCEngine/PlantVsZombie/PlantVsZombie/GameComponents/Behaviors/Zombie/Z_NormalLogicBehavior.cs
@@ -1,4 +1,4 @@
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using PlantVsZombie.GameComponents.GameMessages;
 using SSCEngine.Utils.GameObject.Component;
 using System;
@@ -11,10 +11,18 @@ using PlantVsZombie.GameComponents.Components;
 
 namespace PlantVsZombie.GameComponents.Behaviors.Zombie
 {
+    enum eNormalZombieState
+    {
+        RUNNING, EATING
+    }
     public class Z_NormalLogicBehavior : BaseLogicBehavior
     {
+        private eNormalZombieState ZombieState { get; set; } // Chỉ dùng cục bộ 
+
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
+            // 
+            ZombieState = eNormalZombieState.RUNNING;
             // Die
             LogicComponent logicCOm = this.Owner as LogicComponent;
             if (logicCOm == null)
@@ -32,43 +40,65 @@ namespace PlantVsZombie.GameComponents.Behaviors.Zombie
             if (msg == null)
                 throw new Exception("Z_NormalLogicBehavior: message is not CollisionDetectedMsg");
 
-            NormalPlant plant = message.TargetCollision as NormalPlant;
+            
             // If no collision
             if (message.TargetCollision == null)
             {
-                MoveBehaviorChangeMsg moveMsg1 = new MoveBehaviorChangeMsg(MessageType.CHANGE_MOVE_BEHAVIOR, this);
-                moveMsg1.MoveBehaviorType = Components.eMoveBehaviorType.NORMAL_RUNNING;
-                moveMsg1.DestinationObjectId = this.Owner.Owner.ObjectId;
+                changeRunBehavior(gameTime);
 
-                RenderBehaviorChangeMsg renderMsg1 = new RenderBehaviorChangeMsg(MessageType.CHANGE_RENDER_BEHAVIOR, this);
-                renderMsg1.RenderBehaviorType = GameComponents.Components.eMoveRenderBehaviorType.ZO_NORMAL_RUNNING;
-                renderMsg1.DestinationObjectId = this.Owner.Owner.ObjectId;
-
-                PZObjectManager.Instance.SendMessage(moveMsg1, gameTime);
-                PZObjectManager.Instance.SendMessage(renderMsg1, gameTime);
+                ZombieState = eNormalZombieState.RUNNING;
                 return;
             }
+
+            NormalPlant plant = message.TargetCollision as NormalPlant;
 
             // Send message change to 
             if (plant != null)
             {
-                MoveBehaviorChangeMsg moveMsg = new MoveBehaviorChangeMsg(MessageType.CHANGE_MOVE_BEHAVIOR, this);
-                moveMsg.MoveBehaviorType = Components.eMoveBehaviorType.STANDING;
-                moveMsg.DestinationObjectId = this.Owner.Owner.ObjectId;
+                changeEatBehavivor(gameTime);
 
-                RenderBehaviorChangeMsg renderMsg = new RenderBehaviorChangeMsg(MessageType.CHANGE_RENDER_BEHAVIOR, this);
-                renderMsg.RenderBehaviorType = GameComponents.Components.eMoveRenderBehaviorType.ZO_NORMAL_EATING;
-                renderMsg.DestinationObjectId = this.Owner.Owner.ObjectId;
-
-                PZObjectManager.Instance.SendMessage(moveMsg, gameTime);
-                PZObjectManager.Instance.SendMessage(renderMsg, gameTime);
+                ZombieState = eNormalZombieState.EATING;
 
                 LogicComponent logicCOm = plant.GetComponent(typeof(LogicComponent)) as LogicComponent;
                 if (logicCOm == null)
                     throw new Exception("Z_NormalLogicBehavior: Expect Target Logic Component");
                 logicCOm.Health--;
             }
+            else
+            {
+                if (ZombieState == eNormalZombieState.RUNNING)
+                    changeRunBehavior(gameTime);
+            }
+
             base.OnCollison(msg, gameTime);
+        }
+
+        private void changeEatBehavivor(GameTime gameTime)
+        {
+            MoveBehaviorChangeMsg moveMsg = new MoveBehaviorChangeMsg(MessageType.CHANGE_MOVE_BEHAVIOR, this);
+            moveMsg.MoveBehaviorType = Components.eMoveBehaviorType.STANDING;
+            moveMsg.DestinationObjectId = this.Owner.Owner.ObjectId;
+
+            RenderBehaviorChangeMsg renderMsg = new RenderBehaviorChangeMsg(MessageType.CHANGE_RENDER_BEHAVIOR, this);
+            renderMsg.RenderBehaviorType = GameComponents.Components.eMoveRenderBehaviorType.ZO_NORMAL_EATING;
+            renderMsg.DestinationObjectId = this.Owner.Owner.ObjectId;
+
+            PZObjectManager.Instance.SendMessage(moveMsg, gameTime);
+            PZObjectManager.Instance.SendMessage(renderMsg, gameTime);
+        }
+
+        private void changeRunBehavior(GameTime gameTime)
+        {
+            MoveBehaviorChangeMsg moveMsg1 = new MoveBehaviorChangeMsg(MessageType.CHANGE_MOVE_BEHAVIOR, this);
+            moveMsg1.MoveBehaviorType = Components.eMoveBehaviorType.NORMAL_RUNNING;
+            moveMsg1.DestinationObjectId = this.Owner.Owner.ObjectId;
+
+            RenderBehaviorChangeMsg renderMsg1 = new RenderBehaviorChangeMsg(MessageType.CHANGE_RENDER_BEHAVIOR, this);
+            renderMsg1.RenderBehaviorType = GameComponents.Components.eMoveRenderBehaviorType.ZO_NORMAL_RUNNING;
+            renderMsg1.DestinationObjectId = this.Owner.Owner.ObjectId;
+
+            PZObjectManager.Instance.SendMessage(moveMsg1, gameTime);
+            PZObjectManager.Instance.SendMessage(renderMsg1, gameTime);
         }
 
         
